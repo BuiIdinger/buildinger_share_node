43939bef4bb1194b0d375b3ac75a0bc276770f43
Shayden seymour <shaydenseymour@gmail.com>
2025-05-19 12:03:25 +1200

    Implement get short date function

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,6 +1,20 @@
 #include "Utilities.h"
 #include <chrono>
+#include <string>
+#include <sstream>
 
 int64_t utilities::getCurrentEpoch() {
   return std::chrono::system_clock::now().time_since_epoch().count();
+}
+
+std::string getShortDate() {
+  const std::time_t now = std::time(nullptr);
+  const std::tm* localTime = std::localtime(&now);
+
+  std::ostringstream oss;
+  oss << localTime->tm_mday << "-"
+      << (localTime->tm_mon + 1) << "-"
+      << (localTime->tm_year + 1900);
+
+  return oss.str();
 }
--- src/include/Utilities.h
+++ src/include/Utilities.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <cstdlib>
+#include <string>
 
 namespace utilities {
   int64_t getCurrentEpoch();
+
+  // Returns short date as d-m-y
+  std::string getShortDate();
 }
